aece73efda29a1f038e454a015147bd46d71eb79
YeeHeng <lauyh87@gmail.com>
2021-02-11 02:11:31 +0800

    update

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
+'use strict'
+
+/// import lib
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var pino = require('express-pino-logger')()
 
+// import route
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-
+app.use(pino)
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
